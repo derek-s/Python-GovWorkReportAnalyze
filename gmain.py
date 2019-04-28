239881c992c156d3864b3b7de85333eeeeba8b0b
Derek.S <derekseli@outlook.com>
2019-04-28 15:27:10 +0800

    添加抓取全文函数

--- gmain.py
+++ gmain.py
@@ -17,10 +17,10 @@ def getAllReportUrls(url):
     """
     reportUrls = {}
 
-    allUrlHtmlSource = getHtml(url)
+    html = getHtml(url)
 
     # bs4 html解析器
-    soup = bs(allUrlHtmlSource, "html.parser")
+    soup = bs(html, "html.parser")
 
     # 获取页面内url数据的表格
     yearsTable = soup.select("#UCAP-CONTENT table tbody")[0]
@@ -34,9 +34,35 @@ def getAllReportUrls(url):
     return reportUrls
 
 
+def getReportText(url):
+    """
+    获取公告全文的文字内容
+    :param url: 全文网页连接
+    :return: 文本内容
+    """
+
+    html = getHtml(url)
+
+    soup = bs(html, "html.parser")
+
+    # 针对1954-2013年全文的抓取
+    article = soup.select(".p1")
+    if len(article) == 0:
+        # 针对2014-2017年全文的抓取
+        article = soup.select("#UCAP-CONTENT")
+        if len(article) == 0:
+            # 针对2018、2019年全文的抓取
+            article = soup.select(".zhj-bbqw-cont")
+            if len(article) == 0:
+                raise RuntimeError("抓取内容失败")
+    return article[0].text
 
 
 if __name__ == "__main__":
     urls = getAllReportUrls("http://www.gov.cn/guoqing/2006-02/16/content_2616810.htm")
+    urls["2017"] = "http://www.gov.cn/premier/2017-03/16/content_5177940.htm"
+    urls["2018"] = "http://www.gov.cn/zhuanti/2018lh/2018zfgzbg/zfgzbg.htm"
+    urls["2019"] = "http://www.gov.cn/zhuanti/2019qglh/2019lhzfgzbg/index.htm"
     for itemUrl in urls:
-        print(urls[itemUrl])
+         print(itemUrl)
+         getReportText(urls[itemUrl])
